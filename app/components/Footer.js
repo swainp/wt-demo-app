f422a81b2508be893cdcf91dfee094de84adf353
AugustoL <augusto.lemble@gmail.com>
2017-11-16 23:08:22 -0300

    Add footer links

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,8 +6,8 @@ export default class App extends React.Component {
       return(
         <footer class="footer">
           <div class="container">
-            <img src="assets/wt.svg" style={{height: "24px", marginRight: "0.5em"}} />
-              Winding Tree - <a href="#"> Github </a> - <a href="#"> Documentation </a>
+            <a href="https://windingtree.com"> <img src="assets/wt.svg" style={{height: "24px", marginRight: "0.5em"}} /> Winding Tree </a>
+            - <a href="https://github.com/windingtree"> Github </a> - <a href="https://windingtree.github.io/wt-js-libs/"> Documentation </a>
           </div>
         </footer>
       );
