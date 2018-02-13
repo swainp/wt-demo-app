bcd5f56eaa1da39642d88d07910911d60dc766a2
Arseniy Klempner <adklempner@gmail.com>
2018-02-13 11:15:18 -0800

    Redeploy LifTokenTest

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "WEB3_PROVIDER": "https://ropsten.infura.io/WKNyJ0kClh8Ao5LdmO7z",
-  "LIFTOKEN_ADDRESS": "0x4e9deee821a923e43ff076c4c57a3668938e664b",
+  "LIFTOKEN_ADDRESS": "0x31d2b9cb3bb5f686d561d96adb1ecc1720b48f0e",
   "WT_INDEXES": [
     {
       "version": "0.0.1 (Ropsten)",
