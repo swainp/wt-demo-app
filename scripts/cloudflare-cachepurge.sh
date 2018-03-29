6cd9bf98d1576ec7958e0b8e0be96c3c14301c68
Jirka Chadima <chadima.jiri@gmail.com>
2018-03-29 14:51:57 +0200

    chore: Configures encrypted Cloudflare credentials

--- scripts/cloudflare-cachepurge.sh
+++ scripts/cloudflare-cachepurge.sh
@@ -9,10 +9,11 @@ echo "Purging cloudflare cache for index.html"
 # CF_API_KEY=123456 #   API key generated on the "My Account" page in cloudflare
 # CF_ROOT=https://demo.example.com # Actual domain name that is used in cloudflare
 
-curl -v -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE/purge_cache" \
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE/purge_cache" \
 -H "X-Auth-Email: $CF_USER" \
 -H "X-Auth-Key: $CF_API_KEY" \
 -H "Content-Type:application/json" \
---data "{\"files\": [\"$CF_ROOT\"]}" \
+--data "{\"files\": [\"$CF_ROOT\"]}"
+
 
 echo "Cloudflare cache purged, domain should serve fresh index.html now"
